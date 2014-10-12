878db9652663e7f7c8e28e05a2a45d312eef5650
Jordan Carroll <jcarroll2007@gmail.com>
2014-10-12 09:27:39 -0400

    Updated Profile picture ui and added some logic to user.js, next implement  registration complete and services

--- faces/user.js
+++ faces/user.js
@@ -1,9 +1,27 @@
-var app = angular.module('FacesUser', []);
+var app = angular.module('user', []);
 
 app.factory('UserService', [function() {
-	var UserService = // Here is where we will write the ajax code
+	return {
+		post_user: function() {
+			console.log('creating new user not implemented');
+		},
+
+		update_user: function(id) {
+			console.log('updating user' + id  + 'not implemented');
+		},
+
+		get_user: function(uName, password) {
+			console.log('Get user not implemented');
+		}
+	};
 }]);
 
-app.service('User', function() {
-	// This will be the singleton that will represent the current user
+app.service('user', function() {
+	this.firstName;
+	this.lastName;
+	this.email;
+	this.password;
+	this.dateOfBirth;
+	this.aboutUser;
+	this.profilePictureUrl;
 });
