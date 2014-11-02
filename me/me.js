4f4dddc0bce34a7926ea2622dc87d0bbe612cdb0
ryanmccarthy <ryhmccarthy@gmail.com>
2014-11-02 18:39:24 -0500

    added friends/post/picture area; small stylization

--- me/me.js
+++ me/me.js
@@ -1,24 +1,36 @@
-var app = angular.module('Faces_Me', []);
+var app = angular.module('Faces_Me', ['ui.bootstrap', 'ngAnimate']);
 
 app.controller('MeCtrl' , ['$scope', '$window', 'user', 'post',
 	function($scope, $window, user, post) {
-		var w = angular.element($window);
 		
-		w.bind('resize', function () {
-			console.log('resize');
-		});
-
 		$scope.user = user.user;
 
 		$scope.posts = [];
 
 		$scope.testComments = [];
 
-		$scope.testPost = new post(user, '', 'This is my wall post', $scope.testComments);
+		//$scope.testPost = new post(user, '', 'This is my wall post', $scope.testComments);
 
-		$scope.posts.push($scope.testPost);
+		//$scope.posts.push($scope.testPost);
 
 		$scope.addPosts = function(posts) {
 			$scope.posts.concat(posts);
 		};
+
+		var revealPostBox = false;
+		var revealPictureBox = false;
+
+		$scope.postBoxState = function(){
+			$scope.revealPostBox = !$scope.revealPostBox;
+			$scope.revealPictureBox = false;
+		}
+
+		$scope.pictureBoxState = function(){
+			$scope.revealPictureBox = !$scope.revealPictureBox;
+			$scope.revealPostBox = false;
+		}
+
+		$scope.newPost = function(){
+
+		}
 }]);
