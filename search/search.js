16686f7cc071d26f2197d2a1c34b8d4dd62da916
Jordan Carroll <jcarroll2007@gmail.com>
2014-11-15 11:50:17 -0500

    Added some search logic.

--- search/search.js
+++ search/search.js
@@ -1,8 +1,8 @@
 var app = angular.module('Faces_Search', ['ui.bootstrap', 'ngAnimate']);
 
-app.controller('SearchCtrl', ['$scope',  '$rootScope', '$http',
-	function($scope, $rootScope, $http) {
-		$rootScope.$on('Search', function(obj, searchParameter) {
+app.controller('SearchCtrl', ['$scope',  '$rootScope', '$http', '$user',
+	function($scope, $rootScope, $http, $user) {
+		$scope.$on('Search', function(obj, searchParameter) {
 			$scope.searchText = searchParameter;
 			console.log(searchParameter);
 		});
@@ -15,6 +15,22 @@ app.controller('SearchCtrl', ['$scope',  '$rootScope', '$http',
 		}();
 		$scope.searchText = function() {
 			return "";
+		};
+
+		$scope.isCurrentUser = function(user) {
+			return (user.Email === $user.user.Email);
+		};
+
+		$scope.addFriend = function(user) {
+			var friendship = {
+				UserId: $user.user.id,
+				FriendId: user.id
+			};
+			$http.post('http://robertryanmorris.com/services/FaceServices/api/Friend', friendship)
+			.success(function() {
+				user.isFriend = true;
+				console.log(user);
+			});
 		};
 	}
 ]);
